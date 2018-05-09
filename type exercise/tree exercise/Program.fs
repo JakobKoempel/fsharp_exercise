1b23b730cd5606dde256b94fb0dac55d72eff8a9
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-10 00:13:16 +0200

    simplified code

--- type exercise/tree exercise/Program.fs
+++ type exercise/tree exercise/Program.fs
@@ -1,12 +1,10 @@
-﻿open System
-
-type Tree = {
+﻿type SortTree = {
     value : int 
-    left : Tree option
-    right : Tree option
+    left : SortTree option
+    right : SortTree option
 } with 
-       member this.insert (num : int) : Tree =
-            let rec f (tree : Tree option) (x : int) : Tree =
+       member this.insert (num : int) : SortTree =
+            let rec f (tree : SortTree option) (x : int) : SortTree =
                 match tree with 
                 | Some t -> if x < t.value then {t with left = Some (f t.left x)}
                             else {t with right = Some (f t.right x)}
@@ -14,15 +12,14 @@ type Tree = {
             f (Some this) num
        
        member this.toList =
-            let rec f (tree : Tree option) (list : int list) : int list =
+            let rec f (tree : SortTree option) (list : int list) : int list =
                 match tree with 
                 | Some t -> f t.left list @ [t.value] @ f t.right list
                 | None -> list
             f (Some this) []
     
-let sortedTree : Tree  = [1 ; 10 ; 15 ; 2 ; 3 ; 8] |> List.fold (fun (acc : Tree) num -> acc.insert num) {value = 8 ; left = None ; right = None}
+let tree : SortTree  = [1 ; 10 ; 15 ; 2 ; 3 ; 8] 
+                       |> List.fold (fun (acc : SortTree) num -> acc.insert num) {value = 8 ; left = None ; right = None}
 
-sortedTree |> printfn "automatic print function of the tree in F# :\n%A \n"
-sortedTree.toList |> printfn "in-order traversal of the sorted tree presented in a list :\n%A"
-
-Console.ReadKey () |> ignore
+tree |> printfn "automatic print function of the tree in F# :\n%A \n"
+tree.toList |> printfn "in-order traversal of the sorted tree presented in a list :\n%A"
